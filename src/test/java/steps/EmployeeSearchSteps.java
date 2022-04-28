85e32976c46b1bcf3132da144a91605a93cee491
chief-halfoat <johnathan.j.hart@gmail.com>
2022-04-27 22:05:55 -0400

    end of class 4

--- src/test/java/steps/EmployeeSearchSteps.java
+++ src/test/java/steps/EmployeeSearchSteps.java
@@ -5,6 +5,7 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import pages.LoginPage;
 import utils.CommonMethods;
 import utils.ConfigReader;
 import utils.Constants;
@@ -17,65 +18,51 @@ public class EmployeeSearchSteps extends CommonMethods {
     public void user_is_navigated_to_hrms_application() {
         openBrowserAndLaunchApplication();
     }
+
     @When("user enters valid admin credentials")
     public void user_enters_valid_admin_credentials() {
 
-        WebElement usernamefield = driver.findElement(By.id("txtUsername"));
-//        usernamefield.sendKeys(ConfigReader.getPropertyValue("username"));
-
-        sendText(usernamefield,ConfigReader.getPropertyValue("username"));
-
-        WebElement passwordField = driver.findElement(By.id("txtPassword"));
-//        passwordField.sendKeys(ConfigReader.getPropertyValue("password"));
-        sendText(passwordField,ConfigReader.getPropertyValue("password"));
+        sendText(login.usernameTextbox, ConfigReader.getPropertyValue("username"));
+        sendText(login.passwordTextbox, ConfigReader.getPropertyValue("password"));
+        click(login.loginBtn);
         driver.manage().timeouts().implicitlyWait(Constants.IMPLICIT_WAIT, TimeUnit.SECONDS);
 
     }
+
     @When("user clicks on login button")
     public void user_clicks_on_login_button() {
-
-        WebElement loginBtn = driver.findElement(By.id("btnLogin"));
-//        loginBtn.click();
-        click(loginBtn);
-//        driver.manage().timeouts().implicitlyWait(Constants.IMPLICIT_WAIT, TimeUnit.SECONDS);
-
+        click(login.loginBtn);
     }
+
     @When("user navigated to employee list page")
     public void user_navigated_to_employee_list_page() {
-
-        WebElement pimOption = driver.findElement(By.id("menu_pim_viewPimModule"));
-        click(pimOption);
-//        pimOption.click();
-
-        WebElement empListOption = driver.findElement(By.id("menu_pim_viewEmployeeList"));
-        click(empListOption);
-//        empListOption.click();
-
+        click(employeeSearchPage.pimOption);
+        click(employeeSearchPage.empListOption);
     }
+
     @When("user enters valid employee id")
     public void user_enters_valid_employee_id() {
-        WebElement searchId = driver.findElement(By.id("empsearch_id"));
-        sendText(searchId,"8510142");
-//        searchId.sendKeys("8510142");
+
+        sendText(employeeSearchPage.idField, "8510142");
     }
+
     @When("user clicks on search button")
     public void user_clicks_on_search_button() {
 
         WebElement searchButton = driver.findElement(By.id("searchBtn"));
         click(searchButton);
-//        searchButton.click();
+
     }
+
     @Then("user is able to see employee information")
     public void user_is_able_to_see_employee_information() {
         System.out.println("Result displayed");
         tearDown();
     }
+
     @When("user enters valid employee name")
     public void user_enters_valid_employee_name() {
-    WebElement searchName = driver.findElement(By.xpath("(//*[@type='text'])[1]"));
-    sendText(searchName,"sumair");
-//    searchName.sendKeys("sumair");
-
+        sendText(employeeSearchPage.nameField, "zubair");
     }
 
 }
---
+++ src/test/java/steps/LoginSteps.java
@@ -0,0 +1,54 @@
+package steps;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import pages.LoginPage;
+import utils.CommonMethods;
+import utils.ConfigReader;
+import utils.Constants;
+
+import java.util.concurrent.TimeUnit;
+
+public class LoginSteps extends CommonMethods {
+    @Then("admin user successfully logged in")
+    public void admin_user_successfully_logged_in() {
+        System.out.println("test passed");
+        tearDown();
+    }
+
+    @When("user enters valid ess username and password")
+    public void user_enters_valid_ess_username_and_password() {
+//        LoginPage login = new LoginPage();
+        sendText(login.usernameTextbox,"tts12345");
+//        WebElement usernamefield = driver.findElement(By.id("txtUsername"));
+//        sendText(usernamefield, "TTS12345");
+//        WebElement passwordField = driver.findElement(By.id("txtPassword"));
+//        sendText(passwordField,"hum@nhrm123");
+        sendText(login.passwordTextbox, "Hum@nhrm123");
+        click(login.loginBtn);
+        driver.manage().timeouts().implicitlyWait(Constants.IMPLICIT_WAIT, TimeUnit.SECONDS);
+    }
+
+    @Then("ess user is successfully logged in")
+    public void ess_user_is_successfully_logged_in() {
+        //LEAVE FOR VALIDATION
+        tearDown();
+    }
+
+    @When("users enters invalid username and password")
+    public void users_enters_invalid_username_and_password() {
+
+        WebElement usernamefield = driver.findElement(By.id("txtUsername"));
+        sendText(usernamefield, "TTS12345");
+        WebElement passwordField = driver.findElement(By.id("txtPassword"));
+        sendText(passwordField,"hum@nhrm");
+        driver.manage().timeouts().implicitlyWait(Constants.IMPLICIT_WAIT, TimeUnit.SECONDS);
+    }
+    @Then("user sees error message on the screen")
+    public void user_sees_error_message_on_the_screen() {
+        //homework - verify error message for this
+        tearDown();
+    }
+}
