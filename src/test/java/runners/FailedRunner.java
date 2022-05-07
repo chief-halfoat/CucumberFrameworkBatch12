9e6191649ff6a4ccca49a7613e409056f1aaa710
chief-halfoat <johnathan.j.hart@gmail.com>
2022-05-07 12:28:45 -0400

    safety

--- src/test/java/runners/FailedRunner.java
+++ src/test/java/runners/FailedRunner.java
@@ -1,5 +1,6 @@
 package runners;
 
+
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
@@ -7,7 +8,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         //features we use to provide the path of all the feature files
-        features = "src/test/resources/features/",
+        features = "@target/failed.txt",
         //glue is where we find the implementations of gherkin steps
         //we provide the path of the package to get all the step definitions
         glue = "steps",
@@ -17,5 +18,8 @@ import org.junit.runner.RunWith;
         plugin = {"html:target/cucumber.html","pretty","json:target/cucumber.json", "rerun:target/text/failed.txt"}
 )
 
-public class RunnerClass {
+
+
+public class FailedRunner {
+
 }
