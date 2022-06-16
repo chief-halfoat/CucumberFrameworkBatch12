96d3fc30cfc54c6638ba165cd29999f205dfc93b
chief-halfoat <johnathan.j.hart@gmail.com>
2022-06-15 20:46:57 -0400

    end of API class 06

--- src/test/java/API/HardCodedExamples.java
+++ src/test/java/API/HardCodedExamples.java
@@ -1,22 +1,27 @@
 package API;
 
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.junit.Assert;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
 import java.sql.SQLOutput;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
+@FixMethodOrder (MethodSorters.NAME_ASCENDING)
 public class HardCodedExamples {
 
     String baseURI = RestAssured.baseURI = "http://hrm.syntaxtechs.net/syntaxapi/api";
-    String token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NTUyNDg1MzUsImlzcyI6ImxvY2FsaG9zdCIsImV4cCI6MTY1NTI5MTczNSwidXNlcklkIjoiMzc3NyJ9.8K5gXYyWBynYBvQso-2H_lGIboeJed803u1OyKZkP0I";
+    String token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NTUzMzQ5NzgsImlzcyI6ImxvY2FsaG9zdCIsImV4cCI6MTY1NTM3ODE3OCwidXNlcklkIjoiMzc3NyJ9.6vZIRiTVX657Iz8tyiMqbPu0n7wzuS5DDR4c5MWxwsE";
     static String employee_id;
     @Test
-    public void createEmployee(){
+    public void acreateEmployee(){
        RequestSpecification request =  given().header("Content-Type","application/json").header("Authorization",token).body("{\n" +
                 "  \"emp_firstname\": \"Wanda\",\n" +
                 "  \"emp_lastname\": \"Maximoff\",\n" +
@@ -37,5 +42,77 @@ public class HardCodedExamples {
         response.then().assertThat().body("Employee.emp_firstname",equalTo("Wanda"));
         employee_id=response.jsonPath().getString("Employee.employee_id");
         System.out.println(employee_id);
+    }
+    @Test
+    public void bgetCreatedEmployee(){
+        RequestSpecification request = given().header("Content-Type","application/json").header("Authorization",token).queryParam("employee_id",employee_id);
+        Response response = request.when().get("/getOneEmployee.php");
+        response.prettyPrint();
+
+        response.then().assertThat().statusCode(200);
+        String tempID = response.jsonPath().getString("employee.employee_id");
+        System.out.println(tempID);
+        Assert.assertEquals(tempID,employee_id);
+    }
+//    @Test
+//    public void cupdateEmployee(){
+//        RequestSpecification request = given().header("Content-Type","application/json").header("Authorization",token).body(
+//                "{\n" +
+//                "  \"employee_id\": \" "+ employee_id + " \",\n" +
+//                        "  \"emp_firstname\": \"edward\",\n" +
+//                        "  \"emp_lastname\": \"sisi\",\n" +
+//                        "  \"emp_middle_name\": \"MS1\",\n" +
+//                        "  \"emp_gender\": \"M\",\n" +
+//                        "  \"emp_birthday\": \"1995-06-12\",\n" +
+//                        "  \"emp_status\": \"confirmed\",\n" +
+//                        "  \"emp_job_title\": \"Manager\"\n" +
+//                        "}");
+//
+//        Response response = request.when().put("/updateEmployee.php");
+//        response.prettyPrint();
+//        response.then().assertThat().statusCode(200);
+//    }
+@Test
+public void cupdateEmployee(){
+    RequestSpecification preparedRequest = given().header("Content-Type", "application/json").
+            header("Authorization", token).
+            body("{\n" +
+                    "        \"employee_id\": \""+ employee_id + "\",\n" +
+                    "        \"emp_firstname\": \"edward\",\n" +
+                    "        \"emp_lastname\": \"sisi\",\n" +
+                    "        \"emp_middle_name\": \"MS1\", \n" +
+                    "        \"emp_gender\": \"M\",       \n" +
+                    "        \"emp_birthday\": \"1995-06-12\", \n" +
+                    "        \"emp_status\": \"confirmed\",       \n" +
+                    "        \"emp_job_title\": \"Manager\"        \n" +
+                    "    }");
+
+    Response response =preparedRequest.when().put("/updateEmployee.php");
+    response.prettyPrint();
+    response.then().assertThat().statusCode(200);
+}
+
+    @Test
+    public void dgetupdatedEmployee (){
+        RequestSpecification request = given().header("Content-Type","application/jason").header("Authorization",token).queryParam("employee_id",employee_id);
+
+        Response response = request.when().get("/getOneEmployee.php");
+        response.prettyPrint();
+        response.then().assertThat().statusCode(200);
+    }
+
+    @Test
+    public void eGetAllEmployees(){
+        RequestSpecification request = given().header("Authorization",token).header("Content-Type","application/json");
+        Response response = request.when().get("getAllEmployees.php");
+        String allEmployees = response.prettyPrint();
+        JsonPath js = new JsonPath(allEmployees);
+        int count = js.getInt("Employees.size()");
+        System.out.println(count);
+
+        for (int i = 0; i < count; i++) {
+            String empId = js.getString("Employees["+i+"].employee_id");
+            System.out.println(empId);
+        }
     }
 }
